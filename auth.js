e4e096f3a266252030fe50b8ae7182e646491966
khahdihdz <34166247+khahdihdz@users.noreply.github.com>
2025-01-29 19:11:27 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,24 @@
 const CLIENT_ID = "422012132533-pgroo786kqrbdq8aj1b791erm4aic29l.apps.googleusercontent.com"; // Thay bằng Client ID của bạn
 const SCOPES = "https://www.googleapis.com/auth/fitness.activity.write";
 
+function onGoogleLoad() {
+    gapi.load('auth2', function () {
+        gapi.auth2.init({
+            client_id: CLIENT_ID,
+            scope: SCOPES
+        });
+    });
+}
+
 function signIn() {
-    gapi.auth2.getAuthInstance().signIn().then(user => {
-        console.log("Đăng nhập thành công!");
+    const authInstance = gapi.auth2.getAuthInstance();
+    if (!authInstance) {
+        console.error("Google API chưa được tải!");
+        return;
+    }
+    
+    authInstance.signIn().then(user => {
+        console.log("Đăng nhập thành công!", user);
     }).catch(err => console.error("Lỗi đăng nhập:", err));
 }
 
